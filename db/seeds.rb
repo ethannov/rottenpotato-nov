05a0874651bf153ce00ad5021b8a69ab5c6afe59
Ubuntu <ubuntu@ip-172-26-4-26.ap-southeast-1.compute.internal>
2023-01-03 09:59:48 +0000

    Changed seeds file to include favorite movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,14 @@
 
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'Frozen II', :rating => 'PG',
+    :release_date => '21-Nov-2019'},
+  {:title => 'Everything Everywhere All at Once', :rating => 'M18',
+    :release_date => '11-Mar-2022'},
+  {:title => 'Heartstopper', :rating => 'P13',
+    :release_date => '22-April-2022'}
+  {:title => 'The House Of Flowers: The Movie', :rating => 'P13',
+    :release_date => '23-June-2021'}
 ]
 
 more_movies.each do |movie|
